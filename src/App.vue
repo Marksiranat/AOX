a70a3f2ff36ba0a60bc2dfeeac27c5d8a5a45d9c
Marksiranat <siranat2546@gmail.com>
2023-02-07 22:01:26 +0700

    ag

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@ import { RouterLink, RouterView } from "vue-router";
               d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2Zm7.283 4.002V12H7.971V5.338h-.065L6.072 6.656V5.385l1.899-1.383h1.312Z"
             />
           </svg>
-          TVMK</a
+          SJM</a
         >
         <button
           class="navbar-toggler"
@@ -60,7 +60,7 @@ import { RouterLink, RouterView } from "vue-router";
                       d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2Zm4.646 6.24v.07H5.375v-.064c0-1.213.879-2.402 2.637-2.402 1.582 0 2.613.949 2.613 2.215 0 1.002-.6 1.667-1.287 2.43l-.096.107-1.974 2.22v.077h3.498V12H5.422v-.832l2.97-3.293c.434-.475.903-1.008.903-1.705 0-.744-.557-1.236-1.313-1.236-.843 0-1.336.615-1.336 1.306Z"
                     />
                   </svg>
-                  MOVIE</RouterLink
+                  MEMES</RouterLink
                 ></a
               >
             </li>
